d475583fd1a2a4429f66d2543b008f420ce6c0a7
Vaclav-Kasimov <104634575+Vaclav-Kasimov@users.noreply.github.com>
2023-03-04 15:20:01 +0300

    Add files via upload

---
+++ Collections/Collections/LAB5.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Collections
+{
+    static class LAB5
+    {
+        //11. Составить программу, которая формирует список L, включив в него по одному разу элементы,
+        //    которые входят одновременно в оба списка L1 и L2.
+
+        public static List<T> Task1<T>(List<T> L1, List<T> L2)
+        {
+            List<T> result = new List<T>();
+            foreach (T current in L1)
+            {
+                if (L2.Contains(current))
+                    result.Add(current);
+            }
+            return result;
+        }
+
+        // в конец непустого списка L добавляет все его элементы, располагая их в обратном порядке
+        //(например, по списку из элементов 1, 2, 3 требуется построить список из элементов 1, 2, 3, 3, 2,1);
+
+        static LinkedList<T> Reverse<T>(LinkedList<T> input)//Получаем лист построенный из элементов исходного в обратном порядке
+        {
+            LinkedList<T> result = new LinkedList<T>();
+            foreach (T current in input)
+                result.AddFirst(current);
+            return result;
+        }
+        public static LinkedList<T> Task2<T>(LinkedList<T> original)
+        {
+            LinkedList<T> result = new LinkedList<T>();
+            foreach (T _current in original)//Копируем входной список в результирующий
+                result.AddLast(_current);
+            foreach (T current in Reverse<T>(original))//Копируем перевернутый входной список в результирующий
+                result.AddLast(current);
+            return result;
+
+        }
+
+
+        //        Есть перечень факультативов в учебном заведении.Студенты группы записались на некоторые
+        //        из этих факультативов.Известно для каждого студента, на какие факультативы он записан.
+        //        Определить:
+        //        • на какие факультативы из перечня ходят все студенты группы;
+        //        • на какие факультативы из перечня ходит хотя бы один студент группы;
+        //        • на какие факультативы из перечня не ходит ни один из студентов группы?
+
+
+        public static bool Task3_All(HashSet<string> students, HashSet<string> facult)//Определяет, ходят ли все студенты на факультатив
+        {
+            bool result = true;
+            foreach (string student in students) { result = result && facult.Contains(student); }
+            return result;
+        }
+
+        public static bool Task3_NoOne(HashSet<string> students, HashSet<string> facult)//Определяет, пустой ли список студентов у факультатива
+        {
+            bool result = true;
+            foreach (string student in students) { result = result && !(facult.Contains(student)); }
+            return result;
+        }
+
+        public static bool Task3_OneOrMore(HashSet<string> students, HashSet<string> facult)//Определяет, ходит ли хоть кто-то на факультатив
+        {
+            return !Task3_NoOne(students, facult);
+        }
+
+        //11. Файл содержит текст на русском языке.Сколько разных букв встречается в тексте?
+        public static int Task4(string path)
+        {
+            HashSet<char> RuLetters = new HashSet<char>() { 'а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я', 'А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З', 'И', 'Й', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ъ', 'Ы', 'Ь', 'Э', 'Ю', 'Я' };
+            int count = 0;
+            StreamReader Reader = new StreamReader(path);
+            char symbol;
+            while (!Reader.EndOfStream)
+            {
+                symbol = (char)Reader.Read();
+                if (RuLetters.Contains(symbol))//Если встречает неудаленную из ХэшСета русских букв букву, то удаляет ее и увеличивает 
+                                               //счетчик встреченных русских букв. У букв Ё нумерация не такая как у остальных
+                                               //Так что их удаляем отдельно если встретим.
+                {
+                    count++;
+                    RuLetters.Remove(symbol);
+                    if (symbol == 'ё')
+                        RuLetters.Remove('Ё');
+                    else
+                    {
+                        if (symbol == 'Ё')
+                            RuLetters.Remove('ё');
+                        else
+                        {
+                            if ((int)symbol > 1071)
+                                RuLetters.Remove((char)((int)symbol - 32));
+                            else
+                                RuLetters.Remove((char)((int)symbol + 32));
+                        }
+
+                    }
+                }
+
+
+                
+            }
+            return count;
+        }
+
+
+
+
+    }
+}
---
+++ Collections/Collections/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Тест задачи 1
+            List<string> List1 = new List<string>() { "Кошка", "Собака", "Хомяк" };
+            List<string> List2 = new List<string>() { "Крыса", "Хомяк", "Кошка" };
+            Console.WriteLine("Задание 1 (листы { \"Кошка\", \"Собака\", \"Хомяк\" } и { \"Крыса\", \"Хомяк\", \"Кошка\" }):");
+            foreach (string current in LAB5.Task1<string>(List1, List2))
+                Console.Write(current + " ");
+
+            //Тест задачи 2
+            Console.WriteLine("\n\nЗадание 2 (исходный лист содержит цифры 0-4):");
+            LinkedList<int> L1 = new LinkedList<int>();
+            for (int i = 0; i < 5; i++)
+                L1.AddLast(i);
+            foreach (int n in LAB5.Task2<int>(L1))
+                Console.Write(n + " ");
+
+
+            //Тест задачи 3
+            Console.WriteLine("\n\nЗадание 3 (Всего 5 студентов: Иванов, Петров, Васечкин, сидоров, Вовочкин. Распределяются рандомно.):");
+            Random rand = new Random();
+            HashSet<string> students = new HashSet<string>() { "Иванов", "Петров", "Васечкин", "Сидоров", "Вовочкин" };
+            HashSet<string> Biology = new HashSet<string>();
+            HashSet<string> Physics = new HashSet<string>();
+            HashSet<string> Fizra = new HashSet<string>();
+            foreach (string student in students)
+            {
+                if (rand.Next(0, 2) == 1)
+                    Fizra.Add(student);
+                if (rand.Next(0, 3) == 1)
+                    Biology.Add(student);
+                if (rand.Next(0, 4) == 1)
+                    Physics.Add(student);
+            }
+            Console.Write("Список студентов, посещающих факультатив по физкультуре: ");
+            foreach (string student in Fizra)
+                Console.Write(student + ", ");
+            Console.Write("\nСписок студентов, посещающих факультатив по биологии: ");
+            foreach (string student in Biology)
+                Console.Write(student + ", ");
+            Console.Write("\nСписок студентов, посещающих факультатив по физике: ");
+            foreach (string student in Physics)
+                Console.Write(student + ", ");
+            Console.Write("\n");
+            Console.Write("\nСписок факультативов, на которые ходят все пять студентов: ");
+            if (LAB5.Task3_All(students, Biology)) Console.Write("биология, "); if (LAB5.Task3_All(students, Physics)) Console.Write("физика, "); if (LAB5.Task3_All(students, Fizra)) Console.Write("физкультура.");
+            Console.Write("\nСписок факультативов, на которые ходит хотя бы один студент: ");
+            if (LAB5.Task3_OneOrMore(students, Biology)) Console.Write("биология, "); if (LAB5.Task3_OneOrMore(students, Physics)) Console.Write("физика, "); if (LAB5.Task3_OneOrMore(students, Fizra)) Console.Write("физкультура.");
+            Console.Write("\nСписок факультативов, на которые никто не ходит: ");
+            if (LAB5.Task3_NoOne(students, Biology)) Console.Write("биология, "); if (LAB5.Task3_NoOne(students, Physics)) Console.Write("физика, "); if (LAB5.Task3_NoOne(students, Fizra)) Console.Write("физкультура.");
+
+
+            //Тест задачи 4
+            Console.Write("\n\nЗадание 4. Уникальных русских букв в тексте: ");
+            Console.Write(LAB5.Task4(@"D:/text.txt"));
+        }
+    }
+}
